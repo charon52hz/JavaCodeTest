18001311fcf42f5443fcc2722ce70288ac5962d9
chz <charon52hz@gmail.com>
2024-03-13 15:20:39 +0800

    回溯算法

---
+++ algorithm/src/main/java/org/chz/backTrack/Combine.java
@@ -0,0 +1,26 @@
+package org.chz.backTrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combine {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backTrack(n,k,1);
+        return res;
+    }
+    private void backTrack(int n, int k, int startIndex){
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=startIndex; i <= n - (k - path.size()) + 1; i++){
+            path.add(i);
+            backTrack(n,k,i+1);
+            path.remove(path.size()-1);
+        }
+    }
+    
+}
